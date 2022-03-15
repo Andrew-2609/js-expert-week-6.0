defbfee0b034df3fb712cb167b497807b2fc51ff
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-14 22:18:54 -0300

    Test: create beforeAll() method and todos for routes.js

---
+++ tests/unit/server/routes.test.js
@@ -0,0 +1,17 @@
+import { jest, expect, describe, test, beforeEach } from '@jest/globals';
+import config from '../../../server/config.js';
+
+const { pages } = config;
+
+describe('#Routes - test suite for API response', () => {
+    beforeEach(() => {
+        jest.restoreAllMocks();
+        jest.clearAllMocks();
+    });
+
+    test.todo('GET / - should redirect to home page');
+    test.todo(`GET /home - should respond with ${pages.homeHTML} file stream`);
+    test.todo(`GET /controller - should respond with ${pages.controllerHTML} file stream`);
+    test.todo(`GET /file.ext - should respond with file stream`);
+    test.todo(`GET /unknown - should respond with 404`)
+});
