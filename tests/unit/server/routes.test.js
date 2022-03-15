7993b9d0df13610d060478c9bfb9edf3772333c8
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-14 23:03:49 -0300

    Test: implement test 'GET / - should redirect to home page'

--- tests/unit/server/routes.test.js
+++ tests/unit/server/routes.test.js
@@ -1,7 +1,9 @@
 import { jest, expect, describe, test, beforeEach } from '@jest/globals';
 import config from '../../../server/config.js';
+import { handler } from '../../../server/routes.js';
+import TestUtil from '../_util/testUtil.js';
 
-const { pages } = config;
+const { pages, location } = config;
 
 describe('#Routes - test suite for API response', () => {
     beforeEach(() => {
@@ -9,7 +11,29 @@ describe('#Routes - test suite for API response', () => {
         jest.clearAllMocks();
     });
 
-    test.todo('GET / - should redirect to home page');
+    test('GET / - should redirect to home page', async () => {
+        const params = TestUtil.defaultHandleParams();
+
+        params.request.method = 'GET';
+        params.request.url = '/';
+
+        /* Unnecessary, since params.response.writeHead is already a jest.fn
+        jest.spyOn(
+            params.response,
+            params.response.writeHead.name
+        ).mockReturnValue();
+        */
+
+        await handler(...params.values());
+
+        expect(params.response.writeHead).toBeCalledWith(
+            302,
+            { 'Location': location.home }
+        );
+
+        expect(params.response.end).toBeCalled();
+    });
+
     test.todo(`GET /home - should respond with ${pages.homeHTML} file stream`);
     test.todo(`GET /controller - should respond with ${pages.controllerHTML} file stream`);
     test.todo(`GET /file.ext - should respond with file stream`);
