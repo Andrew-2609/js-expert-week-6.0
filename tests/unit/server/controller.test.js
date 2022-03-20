afbbd922cbe0126fef6baba0b67a4543fcb5b2f4
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-19 21:28:56 -0300

    Test: implement test 'should create a client stream'

--- tests/unit/server/controller.test.js
+++ tests/unit/server/controller.test.js
@@ -1,6 +1,7 @@
 import { beforeEach, describe, expect, jest, test } from '@jest/globals';
 import { Controller } from '../../../server/controller.js';
 import { Service } from '../../../server/service.js';
+import { logger } from '../../../server/util.js';
 import TestUtil from '../_util/testUtil.js';
 
 describe('# Controller - test suite for intermediate layer', () => {
@@ -29,5 +30,39 @@ describe('# Controller - test suite for intermediate layer', () => {
             stream: mockFileStream,
             type: expectedType
         });
+    });
+
+    test('should create a client stream', async () => {
+        const mockFileStream = TestUtil.generateReadableStream(['anything']);
+        const controller = new Controller();
+        const id = 1;
+        const loggerMessage = `closing connection of ${id}`;
+
+        jest.spyOn(
+            Service.prototype,
+            Service.prototype.createClientStream.name
+        ).mockReturnValue({
+            id: id,
+            clientStream: mockFileStream
+        });
+
+        jest.spyOn(
+            Service.prototype,
+            Service.prototype.removeClientStream.name
+        ).mockReturnValue();
+
+        jest.spyOn(
+            logger,
+            'info'
+        ).mockReturnValue();
+
+        const { stream, onClose } = controller.createClientStream();
+
+        onClose(); // calling onClose function to test its behaviour
+
+        expect(stream).toEqual(mockFileStream);
+        expect(onClose).not.toBeNull();
+        expect(Service.prototype.removeClientStream).toHaveBeenCalled();
+        expect(logger.info).toHaveBeenCalledWith(loggerMessage);
     });
 });
