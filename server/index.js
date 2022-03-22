640d5507ab9c34ec37644a3bfab44c4a585cee1e
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-22 17:59:59 -0300

    Feat: add basic exception handling for Errors and Promises in index.js

--- server/index.js
+++ server/index.js
@@ -3,3 +3,9 @@ import server from './server.js';
 import { logger } from './util.js';
 
 server.listen(config.port).on('listening', () => { logger.info(`Server running at port ${config.port}`) });
+
+// prevent the application from crashing in case an uncaught/unhandled exception occurs
+// uncaught => Errors
+// unhandled => Promises
+process.on('uncaughtException', (error) => logger.error(`An uncaught exception was thrown: ${error.stack || error}`));
+process.on('unhandledRejection', (error) => logger.error(`An unhandled exception was thrown: ${error.stack || error}`));
