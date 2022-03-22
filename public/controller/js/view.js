8a02baec515c42bf31d16d62f034155a4fd6e29e
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-21 21:37:48 -0300

    Feat: implement functions onLoad and changeCommandButtonsVisibility in the view layer

--- public/controller/js/view.js
+++ public/controller/js/view.js
@@ -1,3 +1,15 @@
 export default class View {
-    
-}
+    onLoad() {
+        this.changeCommandButtonsVisibility();
+    }
+
+    changeCommandButtonsVisibility(hide = true) {
+        Array.from(document.querySelectorAll('[name=command]'))
+            .forEach(btn => {
+                const fn = hide ? 'add' : 'remove';
+                btn.classList[fn]('unassigned');
+                function onClickReset() { }
+                btn.onclick = onClickReset;
+            });
+    }
+};
