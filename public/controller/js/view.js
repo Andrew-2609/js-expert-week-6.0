4237f5336deaefcf09aeedc6e3ef3dbed0a65d2d
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-21 22:27:04 -0300

    Feat: implement first functions to handle Start Button click

--- public/controller/js/view.js
+++ public/controller/js/view.js
@@ -1,6 +1,14 @@
 export default class View {
+    constructor() {
+        this.btnStart = document.getElementById('start');
+        this.btnStop = document.getElementById('stop');
+        async function onBtnClick() { };
+        this.onBtnClick = onBtnClick;
+    }
+
     onLoad() {
         this.changeCommandButtonsVisibility();
+        this.btnStart.onclick = this.onStartButtonClicked.bind(this);
     }
 
     changeCommandButtonsVisibility(hide = true) {
@@ -11,5 +19,17 @@ export default class View {
                 function onClickReset() { }
                 btn.onClick = onClickReset;
             });
+    }
+
+    configureOnBtnClick(fn) {
+        this.onBtnClick = fn;
+    }
+
+    async onStartButtonClicked({
+        srcElement: { innerText }
+    }) {
+        const btnText = innerText;
+        console.log({ btnText });
+        await this.onBtnClick(btnText);
     }
 };
